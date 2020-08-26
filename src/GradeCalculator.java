53b73f75bb4ce7e3cde6c2f2ef7e7115e797d6e8
Neena Ugail <nhu21@bath.ac.uk>
2020-08-26 16:03:53 +0100

    first commit

---
+++ src/GradeCalculator.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class GradeCalculator {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the name of the text file containing your grades");
+		
+		String fileName = scanner.nextLine();
+		
+		
+	}
+
+}
